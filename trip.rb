3208d1df24ccbff340b5b9f10dd814b3475ea703
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-18 20:17:42 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -12,13 +12,13 @@ puts "Great! Staying in #{stay} is always wonderful."
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
 "We have some wonderful places to eat here. Where would you like to go?"
-
+eats=gets.chomp
 "Great choice! I love #{eats}!"
 
 "And how many nights will you be here?"
 
 "#{nights} whole nights! You'll have plenty of time to enjoy the area."
-eats=gets.chomp
+
 nights=gets.chomp
 
 # Lastly, puts it all back by interpolating these values in a string.
